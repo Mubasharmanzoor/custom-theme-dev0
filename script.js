6cdbd39134abc05127785d340f5e8d1af12a2699
Mubashir Manzoor <mubasharjut73@gmail.com>
2024-10-21 15:15:08 +0500

    mobile-header

--- script.js
+++ script.js
@@ -38,35 +38,37 @@ const products = [
    
 ];
 
-const latestArrivals = document.getElementById("product0");
-latestArrivals.innerHTML = products.map(({ price, imageSrc, title }, index) => {
-    return `
-        <div class="product">
-            <img src="${imageSrc}" alt="${title}" />
-            <div class="star">
-                <p>${title}</p>
-                <span>${price}</span>
-                <img src="img/star2.png" alt="Star" />
-            </div>
-            <button class="CHOOSE-OPTIONS">CHOOSE OPTIONS</button>
-        </div>
-    `;
-}).join('');
-   
+// const latestArrivals = document.getElementById("product0");
+// latestArrivals.innerHTML = products.map(({ price, imageSrc, title }, index) => {
+//     return `
+//         <div class="product">
+//             <img src="${imageSrc}" alt="${title}" />
+//             <div class="star">
+//                 <p>${title}</p>
+//                 <span>${price}</span>
+//                 <img src="img/star2.png" alt="Star" />
+//             </div>
+//             <button class="CHOOSE-OPTIONS">CHOOSE OPTIONS</button>
+//         </div>
+//     `;
+// }).join('');
 
-
-function myFunction(){
-var x = document.getElementById("menu");
-if(x.style.display == "block"){
-    x.style.display ="none"
-}
-else{
-      x.style.display ="block";
-}
-}
+document.addEventListener('DOMContentLoaded', () => {
+    let navBtns = document.querySelectorAll('#x-id, #open-nav'); // Combined selectors
+    let navMenuMobile = document.querySelector('#menu');
+    navBtns.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            if (document.querySelector("#menu").hasAttribute('open')) {
+                document.querySelector("#menu").removeAttribute('open')
+            } else {
+                document.querySelector("#menu").setAttribute('open')
+            }
+        });
+    });
+});
 
 // x icon??/
-// function onClickMenu(){
-//     document.getElementById("menu").classList.toggle("x-icon")
-//     document.getElementById("x-id").classList.toggle("change")
-// }       
+//  function onClickMenu(){
+//      document.getElementById("menu").classList.toggle("x-icon")
+//      document.getElementById("x-id").classList.toggle("change")
+//  }       
